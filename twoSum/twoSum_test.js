6c15c71d117fe6f85cbda34547f780404acab4c7
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-06-13 10:43:22 -0400

    created a new file to retry twoSum with an optimized algorithm

--- twoSum/twoSum_test.js
+++ twoSum/twoSum_test.js
@@ -1,8 +1,11 @@
-import twoSum from './twoSum.js';
+// import twoSum from './twoSum.js';
+import twoSum from './twoSum2.js';
 
 var test = () => {
 
   console.log(twoSum([2,7,11,15], 9)) // should be [ 0, 1 ]
+
+  console.log(twoSum([3,3], 6)) // should be [ 0, 1 ]
 
   console.log(twoSum([3,2,4], 6)) // should be [ 1, 2 ]
 
