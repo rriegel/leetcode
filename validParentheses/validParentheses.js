e2b9562a2eda88165610aa6aac39cf4c0bcc91a9
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-06-22 18:05:15 -0400

    completed validParentheses in JS

---
+++ validParentheses/validParentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ var isValid = (s) => {
+  var parens = {
+      ")": "(",
+      "}": "{",
+      "]": "["
+  };
+  let stack = [];
+  for (var i = 0; i < s.length; i ++) {
+      if (s[i] === "(" || s[i] === "[" || s[i] === "{") {
+          stack.push(s[i]);
+      } else if (stack[stack.length-1] === parens[s[i]]) {
+          stack.pop();
+      } else {
+          return false;
+      }
+  }
+  return stack.length !== 0 ? false : true;
+};
+
+export default isValid;
---
+++ validParentheses/validParentheses_test.js
@@ -0,0 +1,17 @@
+import isValid from './validParentheses.js';
+
+var case1 = "()";
+var case2 = "()[]{}";
+var case3 = "(]";
+var case4 = "([)]";
+var case5 = "{[]}";
+
+var test = () => {
+  console.log(isValid(case1) === true);
+  console.log(isValid(case2) === true);
+  console.log(isValid(case3) === false);
+  console.log(isValid(case4) === false);
+  console.log(isValid(case5) === true);
+};
+
+test();
