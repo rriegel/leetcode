f0bd3aa3fdb9876c91268e73fa907639786188a8
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-06-23 22:35:06 -0400

    this is the commit message

--- addTwoNumbers/addTwoNumbers.js
+++ addTwoNumbers/addTwoNumbers.js
@@ -14,32 +14,27 @@ var addTwoNumbers = function(l1, l2) {
 
   // traversal of lists
   while (l1 || l2) {
-      let l1Value = l1 ? l1.val : 0;
-      let l2Value = l2 ? l2.val : 0;
-
-      let sum = l1Value + l2Value + carry;
-      carry = 0;
-      let newVal = sum;
-
-      if (sum > 9) {
-          newVal = sum % 10;
-          carry = 1;
-      }
-
-      node.next = new ListNode(newVal);
-      node = node.next;
-
-      if (l1) {
-          l1 = l1.next
-      }
-      if (l2) {
-          l2 = l2.next
-      }
+    let l1Value = l1 ? l1.val : 0;
+    let l2Value = l2 ? l2.val : 0;
+    let sum = l1Value + l2Value + carry;
+    carry = 0;
+    let newVal = sum;
+    if (sum > 9) {
+      newVal = sum % 10;
+      carry = 1;
+    }
+    node.next = new ListNode(newVal);
+    node = node.next;
+    if (l1) {
+        l1 = l1.next
+    }
+    if (l2) {
+        l2 = l2.next
+    }
   }
   if (carry) {
-      node.next = new ListNode(carry);
+    node.next = new ListNode(carry);
   }
-
   return head.next;
 };
 
