862054846c06994459c0c362144e51158b5a8ff9
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-06-28 08:56:01 -0400

    reorganized my solution to lengthOfLongestSubstring

--- lengthOfLongestSubstring/lengthOfLongestSubstring.js
+++ lengthOfLongestSubstring/lengthOfLongestSubstring.js
@@ -3,27 +3,18 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-  let largest = 0;
-  let obj = {};
-  let firstLetter = 0;
-  // as you loop through the string, store the letters in an object
+  let largest = 0, obj = {}, firstLett = 0;
+
   for (let i = 0; i < s.length; i ++) {
-    // if the current letter already exists in the object
     if (obj[s[i]]) {
       let newLength = Object.keys(obj).length;
-      // IF it is larger than the value of largest
       if (newLength > largest) {
-        // store the number of keys as largest
         largest = newLength;
       }
-      // reset the obj
-      firstLetter += 1;
-      i = firstLetter;
-      obj = {};
+      firstLett += 1;
+      i = firstLett;
       obj = {[s[i]]: i};
-    // otherwise
     } else {
-      // keep track of current letter in obj
       obj[s[i]] = i;
     }
   }
