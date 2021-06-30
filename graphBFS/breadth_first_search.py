f87d06528acbca19056edd4d6ed4d0d26ae28d84
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-06-30 19:23:50 -0400

    finished BFS

---
+++ graphBFS/breadth_first_search.py
@@ -0,0 +1,45 @@
+"""
+Grokking Algorithms Ch 6: Breadth-First Search
+"""
+
+## Implementing the graph
+### Using a hash table (python dictionary) and arrays
+graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["thom"] = []
+graph["jonny"] = []
+
+## Implementing the algorithm
+### First make a queue
+from collections import deque
+
+## evaluation function
+def person_is_seller(name):
+  return name[-1] == 'm'
+
+## Breadth-first search
+## O(V+E)
+  ## V = number of vertices
+  ## E = number of edges
+def search(name):
+  search_queue = deque()
+  search_queue += graph[name]
+  searched = [] ## keep track of which people were already searched
+  while search_queue:
+    person = search_queue.popleft()
+    if not person in searched: ## only search this person if they haven't already been searched
+      if person_is_seller(person):
+        print(person + " is a mango seller")
+        return True
+      else:
+        print(person)
+        search_queue += graph[person]
+        searched.append(person) ## mark person as searched
+  return False
+
+search("you")
