92df5fc790b0213fab91abbe0b2183574e8e097c
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-07-05 10:01:47 -0400

    completed allLongestStrings with Big O(n^2)

---
+++ allLongestStrings/allLongestStrings.js
@@ -0,0 +1,12 @@
+function allLongestStrings(inputArray) {
+  // var to track longest string length
+  // object to hold strings and l for quick return as array using object.keys
+  // loop
+  let maxL = 0, obj = {};
+
+  for (let i = 0; i < inputArray.length; i ++) {
+      let currentL = inputArray[i].length;
+      if (currentL > maxL) { maxL = currentL};
+  }
+  return inputArray.filter(str => str.length === maxL);
+}
