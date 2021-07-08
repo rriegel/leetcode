8cc6a8fca831393253615e6f2d57f9943c8b3b7f
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-07-08 17:04:17 -0400

    completed addBorder in js

---
+++ addBorder/addBorder.js
@@ -0,0 +1,27 @@
+/*
+Given a rectangular matrix of characters, add a border of asterisks(*) to it.
+
+Example:
+
+For:
+picture = ["abc",
+           "ded"]
+
+the output should be:
+addBorder(picture) = ["*****",
+                      "*abc*",
+                      "*ded*",
+                      "*****"]
+*/
+
+function addBorder(picture) {
+  // get length of first row and add 2 to get length of asterisk rows
+  let l = picture[0].length + 2;
+  let ends = "*".repeat(l);
+  for (let i = 0; i < picture.length; i ++) {
+      picture[i] = "*" + picture[i] + "*";
+  }
+  picture.unshift(ends);
+  picture.push(ends);
+  return picture;
+}
