f5156ebdd1c1633d316f131ee1d282eb561c815d
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-07-11 20:47:44 -0400

    completed areSimilar in js

---
+++ areSimilar/areSimilar.js
@@ -0,0 +1,17 @@
+function areSimilar(a, b) {
+  if (a.toString() == b.toString()) {
+      return true;
+  }
+  let c = [], d = [];
+  for (let i = 0; i < a.length; i ++) {
+      if (a[i] !== b[i]) {
+          c.push(a[i]);
+          d.push(b[i]);
+      }
+  }
+  c = c.reverse();
+  if (c.length === 2 && (c.toString() == d.toString())) {
+      return true;
+  }
+  return false;
+}
