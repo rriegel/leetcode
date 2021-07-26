80f30e86bcbc1dafdd0881840264ecd24e4acffa
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-07-26 16:38:45 -0400

    playing around with performance.now() in node.js

--- JSDSAmasterclass/bigO/add1.js
+++ JSDSAmasterclass/bigO/add1.js
@@ -1,3 +1,5 @@
+const {performance} = require('perf_hooks');
+
 function addUpTo(n) {
   let total = 0;
   for (let i = 1; i <= n; i ++) {
@@ -8,3 +10,8 @@ function addUpTo(n) {
 
 console.log(addUpTo(5));
 console.log(addUpTo(100));
+
+let t1 = performance.now();
+addUpTo(1000000000);
+let t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
