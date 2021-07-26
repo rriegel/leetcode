e33afeeeb994c24a0531ce331de9485e896aed00
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-07-26 15:48:17 -0400

    completed myPow function with naive solution, d&c solution and added memoization to improve performance with large numbers

---
+++ googlePrep/pow/pow.js
@@ -0,0 +1,51 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+ var myPow = function(x, n) {
+
+  // // NAIVE SOLUTION
+  // // negative case
+  // if (n < 0) {
+  //     n = Math.abs(n);
+  //     x = 1 / x;
+  // }
+  // var total = 1;
+  // while (n > 0) {
+  //     total = total * x;
+  //     n --;
+  // }
+  // return total;
+
+  // DIVIDE AND CONQUER SOLUTION WITH MEMOIZATION
+  // negative case
+  if (n < 0) {
+      n = Math.abs(n);
+      x = 1 / x;
+  }
+
+  let calc = new Map();
+
+  var recursePow = (num, pow) => {
+      if (pow === 1) { return num };
+      if (calc.has(pow)) { return calc.get(pow) };
+
+      let val1 = Math.floor(pow / 2);
+      let val2 = pow - val1;
+      calc.set(pow, recursePow(num, val1) * recursePow(num, val2));
+      return calc.get(pow);
+  };
+
+  let res = n > 0 ? recursePow(x, n) : 1;
+  return res;
+};
+
+/*
+             2^6
+          2^3 * 2^3
+       2^2 * 2^2 * 2^2
+2^1 * 2^1 * 2^1 * 2^1 * 2^1 * 2^1
+*/
+
+export default myPow;
---
+++ googlePrep/pow/pow_test.js
@@ -0,0 +1,7 @@
+import myPow from './pow(x,n).js';
+
+var test = () => {
+  console.log(myPow(3, 6));
+}
+
+test();
