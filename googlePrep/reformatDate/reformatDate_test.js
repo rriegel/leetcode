393c686f28856fc1a00690bdfcfc86c0adecf8c7
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-07-27 16:32:00 -0400

    made tests more precise

--- googlePrep/reformatDate/reformatDate_test.js
+++ googlePrep/reformatDate/reformatDate_test.js
@@ -2,9 +2,9 @@ const reformatDate = require('./reformatDate');
 const {performance} = require('perf_hooks');
 
 var test = () => {
-  console.log(reformatDate('9th Feb 1997'));
-  console.log(reformatDate('4th Nov 1999'));
-  console.log(reformatDate('27th Jul 2021'));
+  console.log(reformatDate('9th Feb 1997') === '1997-02-09');
+  console.log(reformatDate('4th Nov 1999') === '1999-11-04');
+  console.log(reformatDate('27th Jul 2021') === '2021-07-27');
 
   let t1 = performance.now();
   reformatDate('9th Feb 1997')
