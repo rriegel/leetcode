f49a41614decd044996fa3c45b3e2ff41f9ca6fe
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-07-30 15:42:35 -0400

    helper function collect odds

---
+++ JSDSAmasterclass/recursion/collectOdds.js
@@ -0,0 +1,18 @@
+function collectOdds(nums) {
+  let result = [];
+
+  function helper(input) {
+    if (input.length === 0) {
+      return;
+    }
+    if (input[0] % 2 !== 0) {
+      result.push(input[0])
+    }
+    helper(input.slice(1));
+  }
+  helper(nums);
+
+  return result;
+}
+
+console.log(collectOdds([1,2,3,4,5,6,7,8,9]));
