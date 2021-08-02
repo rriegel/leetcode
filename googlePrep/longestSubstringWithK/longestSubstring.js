473a661f8ec37d1b0bd1708f5ead22926c1a9d64
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-08-02 12:58:39 -0400

    started longestSubstring.js

---
+++ googlePrep/longestSubstringWithK/longestSubstring.js
@@ -0,0 +1,25 @@
+/*
+Given a string S, find the length of the longest substring T that contains at most k distinct characters.
+
+Example
+Example 1:
+
+Input: S = "eceba" and k = 3
+Output: 4
+Explanation: T = "eceb"
+Example 2:
+
+Input: S = "WORLD" and k = 4
+Output: 4
+Explanation: T = "WORL" or "ORLD"
+*/
+/*
+i - str and k
+o - number that indicates the longest substring with k distinct characters
+c - N/A
+e - empty string / k is larger than str length
+*/
+// strategy - hashing and sliding window
+function longestSubstring(str, k) {
+  //
+};
