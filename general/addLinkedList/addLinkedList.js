2b9b42bda12a09fd9656146ba1b8778d72f684f3
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-08-01 22:27:31 -0400

    adjusted addLinkedList solution

--- general/addLinkedList/addLinkedList.js
+++ general/addLinkedList/addLinkedList.js
@@ -19,27 +19,31 @@ function ListNode(val = null, next = null) {
 function addTwoNumbers(l1, l2) {
   let res = new ListNode();
   let node = res;
-  let carry;
-  while (l1 && l2) {
-    carry = 0;
-    let sum = l1.val + l2.val;
+  let carry = 0;
+  while (l1 || l2) {
+    let l1val = l1 ? l1.val : 0;
+    let l2val = l2 ? l2.val : 0;
 
+    let sum = l1val + l2val + carry;
+    carry = 0;
     if (sum > 9) {
-      let tmp = sum.toString();
-      carry = Number(tmp.substring(0, tmp.length - 1));
+      carry = 1;
       sum = sum % 10;
     }
-    node.val = node.val + sum;
-    node.next = new ListNode(carry);
 
+    node.next = new ListNode(sum);
     node = node.next;
-    l1 = l1.next;
-    l2 = l2.next;
+    if (l1) {
+      l1 = l1.next
+    }
+    if (l2) {
+        l2 = l2.next
+    }
   }
   if (carry > 0) {
-    node.val = carry;
+    node.next = new ListNode(carry);
   }
-  return res;
+  return res.next;
 };
 
 function test() {
