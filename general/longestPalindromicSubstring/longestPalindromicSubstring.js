018b9c856f629d8d2c3ddb03d8ea7bb8379a1838
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-08-04 10:04:38 -0400

    completed longestPalindromicSubstring

--- general/longestPalindromicSubstring/longestPalindromicSubstring.js
+++ general/longestPalindromicSubstring/longestPalindromicSubstring.js
@@ -9,3 +9,32 @@ Input: "million"
 Output: "illi"
 */
 
+function longestPalindrome(s) {
+  let longestPal = '';
+
+  var getLongestPalindrome = function (lPos, rPos) {
+    while (
+      lPos >= 0 &&
+      rPos < s.length &&
+      s[lPos] === s[rPos]
+    ) {
+      lPos--;
+      rPos++;
+    }
+    if (rPos - lPos > longestPal.length) {
+      longestPal = s.slice(lPos+1, rPos);
+    }
+  };
+
+  for (let i = 0; i < s.length; i ++) {
+    // find longest odd palindrome
+    getLongestPalindrome(i, i+1);
+    // find longest even palindrome
+    getLongestPalindrome(i, i);
+
+    if ((s.length - i) * 2 < longestPal.length) {
+      break;
+    }
+  }
+  return longestPal;
+}
