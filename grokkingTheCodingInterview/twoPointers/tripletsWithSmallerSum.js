3c573d1004da6cb27e182c1cb16b4563262eea5c
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-08-06 15:34:32 -0400

    changed code to allow repeats of elements bc that should be taken into account

--- grokkingTheCodingInterview/twoPointers/tripletsWithSmallerSum.js
+++ grokkingTheCodingInterview/twoPointers/tripletsWithSmallerSum.js
@@ -20,25 +20,24 @@ function smallerSumTriplets(nums, target) {
   nums = nums.sort((a,b) => a-b);
   let count = 0;
   for (let left = 0; left < nums.length-2; left ++) {
-    if (left === 0 || nums[left] !== nums[left-1]) {
-      let mid = left+1;
-      let right = nums.length-1;
-      while (mid < right) {
-        let currSum = nums[left] + nums[mid] + nums[right];
-        if (currSum < target) {
-          // instead of incrementing the count once, increment it by right - mid
-          // because all sums in between will be smaller than the current one
-          // meaning they satisfy the condition
-          count += right - mid;
-          mid ++;
-        } else right --;
-      }
+    let mid = left+1;
+    let right = nums.length-1;
+    while (mid < right) {
+      let currSum = nums[left] + nums[mid] + nums[right];
+      if (currSum < target) {
+        // instead of incrementing the count once, increment it by right - mid
+        // because all sums in between will be smaller than the current one
+        // meaning they satisfy the condition
+        count += right - mid;
+        mid ++;
+      } else right --;
     }
   }
   return count;
 };
 
 function test() {
+  debugger;
   console.log(smallerSumTriplets([-1, 0, 2, 3], 3) === 2);
   console.log(smallerSumTriplets([-1, 4, 2, 1, 3], 5) === 4);
   console.log(smallerSumTriplets([-1, 0, 2, 3, 6, 8, 1, 1], 6) === 19);
