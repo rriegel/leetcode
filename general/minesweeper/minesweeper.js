ed3ea6dc9445c921cb6c18c4d9beb591d4811b3c
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-08-08 11:58:49 -0400

    completed codeSignal minesweeper.js

---
+++ general/minesweeper/minesweeper.js
@@ -0,0 +1,20 @@
+function minesweeper(matrix) {
+  let newMatrix = [];
+  for (let n = 0; n < matrix.length; n ++) {
+    newMatrix[n] = [];
+    for (let m = 0; m < matrix[n].length; m ++) {
+      let currSquare = matrix[n][m];
+      let mineCount = 0;
+      if (n > 0 && matrix[n-1][m] === true) mineCount ++;
+      if (m > 0 && matrix[n][m-1] === true) mineCount ++;
+      if (n < matrix.length-1 && matrix[n+1][m] === true) mineCount ++;
+      if (m < matrix[n].length-1 && matrix[n][m+1] === true) mineCount ++;
+      if (m > 0 && n > 0 && matrix[n-1][m-1] === true) mineCount ++;
+      if (m > 0 && n < matrix.length-1 && matrix[n+1][m-1] === true) mineCount ++;
+      if (m < matrix[n].length-1 && n < matrix.length-1 && matrix[n+1][m+1] === true) mineCount ++;
+      if (m < matrix[n].length-1 && n > 0 && matrix[n-1][m+1] === true) mineCount ++;
+      newMatrix[n][m] = mineCount;
+    }
+  }
+  return newMatrix;
+}
