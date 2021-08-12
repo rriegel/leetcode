e0f3aa981f779f038808f805bcd2b4a1b10b19ec
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-08-12 16:39:17 -0400

    created a file for the treeNode class to make the code more modular

--- grokkingTheCodingInterview/treeBFS/binaryTreeLevelTraversal.js
+++ grokkingTheCodingInterview/treeBFS/binaryTreeLevelTraversal.js
@@ -4,14 +4,7 @@ Binary Tree Level Order Traversal (easy)
 Given a binary tree, populate an array to represent its level-by-level traversal.
 You should populate the values of all nodes of each level from left to right in separate sub-arrays.
 */
-
-class TreeNode {
-  constructor(value = null) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-};
+const TreeNode = require('./treeNode');
 
 function traverse(root) {
   let result = [];
---
+++ grokkingTheCodingInterview/treeBFS/reverseLevelOrderTraversal.js
@@ -0,0 +1,7 @@
+/*
+Reverse Level Order Traversal (easy)
+
+Given a binary tree, populate an array to represent its level-by-level traversal in reverse order, i.e., the lowest level comes first.
+You should populate the values of all nodes in each level from left to right in separate sub-arrays.
+*/
+const TreeNode = require('./treeNode');
