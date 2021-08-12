52dcbae26c4c33e2087e16ee47e416169a4c711b
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-08-12 16:51:15 -0400

    completed reverseLevelOrderTraversal.js

--- grokkingTheCodingInterview/treeBFS/reverseLevelOrderTraversal.js
+++ grokkingTheCodingInterview/treeBFS/reverseLevelOrderTraversal.js
@@ -7,11 +7,33 @@ You should populate the values of all nodes in each level from left to right in 
 const TreeNode = require('./treeNode');
 
 function reverseTraverse(root) {
+  if (root === null) return [];
 
+  let result = [];
+  let queue = [];
+  queue.push(root);
+  while (queue.length) {
+    let levelSize = queue.length;
+    let currentLevel = [];
+    for (let i = 0; i < levelSize; i ++) {
+      let node = queue.shift();
+      currentLevel.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    result.unshift(currentLevel);
+  }
+  return result;
 };
 
 function test() {
-
+  var root = new TreeNode(12);
+  root.left = new TreeNode(7);
+  root.right = new TreeNode(1);
+  root.left.left = new TreeNode(9);
+  root.right.left = new TreeNode(10);
+  root.right.right = new TreeNode(5);
+  console.log(reverseTraverse(root));
 };
 
 test();
