c34630e866b30a911c8fb8561045debbf4974005
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-08-15 11:33:28 -0400

    aadjusted mergeSortedLists

--- general/mergeSortedLists/mergeSortedLists.js
+++ general/mergeSortedLists/mergeSortedLists.js
@@ -1,30 +1,31 @@
 // Definition for singly-linked list.
-export function ListNode(val, next) {
-    this.val = (val===undefined ? 0 : val)
-    this.next = (next===undefined ? null : next)
+function ListNode(val, next) {
+  this.val = (val===undefined ? 0 : val)
+  this.next = (next===undefined ? null : next)
 }
 /**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
- */
-export function mergeTwoLists(l1, l2) {
+*/
+
+function mergeTwoLists(l1, l2) {
   if (!l1) {return l2}
   if (!l2) {return l1}
   let l3 = new ListNode();
   let head = l3;
   while (l1 && l2) {
-      if (l1.val < l2.val) {
-          head.next = l1;
-          l1 = l1.next;
-      } else {
-          head.next = l2;
-          l2 = l2.next;
-      }
-      head = head.next;
+    if (l1.val < l2.val) {
+      head.next = l1;
+      l1 = l1.next;
+    } else {
+      head.next = l2;
+      l2 = l2.next;
+    }
+    head = head.next;
   }
-
   head.next = l1 || l2;
-
   return l3.next;
 };
+
+module.exports = {ListNode, mergeTwoLists};
--- general/mergeSortedLists/mergeSortedLists_test.js
+++ general/mergeSortedLists/mergeSortedLists_test.js
@@ -1,21 +1,22 @@
-import * as fns from './mergeSortedLists.js';
+const {ListNode, mergeTwoLists} = require('./mergeSortedLists');
 
-let l1 = new fns.ListNode(1);
+let l1 = new ListNode(1);
 let head1 = l1;
 
-let l2 = new fns.ListNode(3);
+let l2 = new ListNode(3);
 let head2 = l2;
 
-head1.next = new fns.ListNode(2)
-head2.next = new fns.ListNode(4)
+head1.next = new ListNode(2)
+head2.next = new ListNode(4)
 
 head1 = head1.next;
 head2 = head2.next;
 
-head1.next = new fns.ListNode(5)
+head1.next = new ListNode(5)
 
 function test() {
-  console.log(fns.mergeTwoLists(l1, l2));
+  console.log(l1);
+  console.log(mergeTwoLists(l1, l2));
 };
 
 test();
