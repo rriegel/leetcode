fd01d357a64517bec9ffb9d82a382fdb2fe41a6d
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-08-15 11:35:38 -0400

    adjusted groupAnagrams.js

--- general/groupAnagrams/groupAnagrams.js
+++ general/groupAnagrams/groupAnagrams.js
@@ -6,23 +6,23 @@
 var groupAnagrams = function(strs) {
   let sorted = [];
   for (let i = 0; i < strs.length; i ++) {
-      sorted.push(strs[i].split('').sort().join(''));
+    sorted.push(strs[i].split('').sort().join(''));
   }
   let map = {};
   for (let i = 0; i < sorted.length; i ++) {
-      if (!map[sorted[i]]) {
-          map[sorted[i]] = [i];
-      } else {
-          map[sorted[i]].push(i);
-      }
+    if (!map[sorted[i]]) {
+      map[sorted[i]] = [i];
+    } else {
+      map[sorted[i]].push(i);
+    }
   }
   let result = [];
   for (let key in map) {
-      let currAnagram = [];
-      for (let i = 0; i < map[key].length; i ++) {
-          currAnagram.push(strs[map[key][i]]);
-      }
-      result.push(currAnagram);
+    let currAnagram = [];
+    for (let i = 0; i < map[key].length; i ++) {
+      currAnagram.push(strs[map[key][i]]);
+    }
+    result.push(currAnagram);
   }
   return result;
 };
