aab9ca8e0ad0e2fd0a53c211bb8ae5d47a1a0d4b
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-08-17 12:50:59 -0400

    started treeDFS

---
+++ grokkingTheCodingInterview/treeDFS/binaryTreePathSum.js
@@ -0,0 +1,24 @@
+/*
+Binary Tree Path Sum (easy)
+
+Given a binary tree and a number ‘S’, find if the tree has a path
+from root-to-leaf such that the sum of all the node values of that path equals ‘S’.
+*/
+const TreeNode = require('./treeNode');
+
+function hasPathWithSum(root, S) {
+
+};
+
+function test() {
+  let root = new TreeNode(12);
+  root.left = new TreeNode(7);
+  root.right = new TreeNode(1);
+  root.left.left = new TreeNode(9);
+  root.right.left = new TreeNode(10);
+  root.right.right = new TreeNode(5);
+  console.log(hasPathWithSum(root, 23));
+  console.log(hasPathWithSum(root, 16));
+};
+
+test();
