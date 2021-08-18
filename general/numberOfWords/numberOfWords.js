15f2664ef36b50381cfbacfac65bee7a2616f0d8
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-08-17 22:43:06 -0400

    completed numberOfWords.js

--- general/numberOfWords/numberOfWords.js
+++ general/numberOfWords/numberOfWords.js
@@ -4,10 +4,16 @@ My sample string was a long excerpt from a book.
 */
 
 function countWords(str) {
-  let result = {};
+  const wordMap = {};
 
-  str = str.replace(/[(),.!?-]/, '').split(' ');
-  console.log(str);
+  str = str.replace(/[^\w\s]/gi, '').split(' ');
+  for (let i = 0; i < str.length; i ++) {
+    str[i] = str[i].toLowerCase();
+  }
+  for (let i = 0; i < str.length; i ++) {
+    wordMap[str[i]] = wordMap[str[i]] === undefined ? 1 : wordMap[str[i]] + 1;
+  }
+  console.log(wordMap);
 };
 function test() {
   let excerpt = "Many interviewees find tree and graph problems to be some of the trickiest. Searching a tree is more complicated than searching in a linearly organized data structure such as an array or linked list. Additionally, the worst case and average case time may vary wildly, and we must evaluate both aspects of any algorithm. Fluency in implementing a tree or graph from scratch will prove essential. Because most people are more familiar with trees than graphs (and they're a bit simpler), we'll discuss trees first. This is a bit out of order though, as a tree is actually a type of graph."
