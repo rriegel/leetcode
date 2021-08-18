78824a50b6c70a00535f8080c0883fee2c9dbc53
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-08-17 22:53:58 -0400

    added a test

--- general/numberOfWords/numberOfWords.js
+++ general/numberOfWords/numberOfWords.js
@@ -17,8 +17,8 @@ function countWords(str) {
 };
 function test() {
   let excerpt = "Many interviewees find tree and graph problems to be some of the trickiest. Searching a tree is more complicated than searching in a linearly organized data structure such as an array or linked list. Additionally, the worst case and average case time may vary wildly, and we must evaluate both aspects of any algorithm. Fluency in implementing a tree or graph from scratch will prove essential. Because most people are more familiar with trees than graphs (and they're a bit simpler), we'll discuss trees first. This is a bit out of order though, as a tree is actually a type of graph."
-
-  console.log(countWords(excerpt));
-
+  // console.log(countWords(excerpt));
+  let sentence = "LMFAO I had to watch this multiple times. He was so confident in what he was spitting like he has told this to hella people and no one has proven him wrong."
+  console.log(countWords(sentence));
 };
 test()
