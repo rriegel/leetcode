e62b7f244a2444b4f92b880161c87363b636cbf4
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-08-18 22:17:15 -0400

    completed pathWithGivenSequence.js

---
+++ grokkingTheCodingInterview/treeDFS/pathWithGivenSequence.js
@@ -0,0 +1,45 @@
+/*
+Path With Given Sequence (medium)
+
+Given a binary tree and a number sequence, find if the sequence is present as a root-to-leaf path in the given tree.
+*/
+const TreeNode = require('./treeNode');
+
+function findPath(root, sequence) {
+  if (!root) return false;
+
+  const dfs = (currNode, currSeq, seqIdx) => {
+    if (!currNode) return false;
+
+    currSeq.push(currNode.val);
+
+    if (currSeq[seqIdx] === sequence[seqIdx]) {
+      if (!currNode.left && !currNode.right && currSeq.length === sequence.length) {
+        return true;
+      } else {
+        seqIdx ++;
+        return dfs(currNode.left, currSeq, seqIdx) || dfs(currNode.right, currSeq, seqIdx);
+      }
+    }
+    seqIdx --;
+    currSeq.pop();
+    return false;
+  };
+
+  return dfs(root, [], 0);
+};
+
+function test() {
+  let root = new TreeNode(6);
+  root.left = new TreeNode(8);
+  root.right = new TreeNode(3);
+  root.left.left = new TreeNode(4);
+  root.right.left = new TreeNode(6);
+  root.right.right = new TreeNode(5);
+  console.log(findPath(root, [6,8,4]));
+  console.log(findPath(root, [6,3,6]));
+  console.log(findPath(root, [6,3,5]));
+  console.log(findPath(root, [6,4,5,8]) === false);
+};
+
+test();
