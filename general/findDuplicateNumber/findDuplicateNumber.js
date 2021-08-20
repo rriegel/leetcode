39e5968ba54c2c2b1e0f79cbee700cbecaffdeb2
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-08-20 16:15:22 -0400

    wrote tests for function

--- general/findDuplicateNumber/findDuplicateNumber.js
+++ general/findDuplicateNumber/findDuplicateNumber.js
@@ -1,3 +1,29 @@
+/*
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+
+
+
+Example 1:
+Input: nums = [1,3,4,2,2]
+Output: 2
+
+Example 2:
+Input: nums = [3,1,3,4,2]
+Output: 3
+
+Example 3:
+Input: nums = [1,1]
+Output: 1
+
+Example 4:
+Input: nums = [1,1,2]
+Output: 1
+*/
+
 var findDuplicate = function(nums) {
   let fast = 0, slow = 0;
   while (true) {
@@ -15,7 +41,10 @@ var findDuplicate = function(nums) {
 };
 
 function test() {
-
+  console.log(findDuplicate([1,3,4,2,2]) === 2);
+  console.log(findDuplicate([3,1,3,4,2]) === 3);
+  console.log(findDuplicate([1,1]) === 1);
+  console.log(findDuplicate([1,1,2]) === 1);
 };
 
 test();
