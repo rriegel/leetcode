31bd432d7a04c96639114f14c306f3a13e4af7cf
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-08-20 16:13:10 -0400

    completed findDuplicateNumber.js

---
+++ general/findDuplicateNumber/findDuplicateNumber.js
@@ -0,0 +1,21 @@
+var findDuplicate = function(nums) {
+  let fast = 0, slow = 0;
+  while (true) {
+    fast = nums[nums[fast]];
+    slow = nums[slow];
+    if (fast === slow) {
+      let pointer = 0;
+      while (pointer !== slow) {
+        pointer = nums[pointer];
+        slow = nums[slow];
+      }
+      return pointer;
+    }
+  }
+};
+
+function test() {
+
+};
+
+test();
