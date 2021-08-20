ceb497822288cdd2e0fd76c14ad4de34ca8b497d
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-08-19 23:12:19 -0400

    adjusted var names

--- general/wordSearch/wordSearch.js
+++ general/wordSearch/wordSearch.js
@@ -12,25 +12,24 @@ Example:
 Given this matrix, and the target word FOAM, you should return true, as it can be found going up-to-down in the first column.
 */
 
-function wordSearch(matrix, target) {
-  let yLen = matrix.length;
-  let xLen = matrix[0].length;
+function wordSearch(board, word) {
+  let yLen = board.length;
+  let xLen = board[0].length;
   for (let i = 0; i < yLen; i ++) {
     for (let j = 0; j < xLen; j ++) {
-      if (matrix[i][j] === target[0]) {
+      if (board[i][j] === word[0]) {
         let vertIdx = 1;
         let horizIdx = 1;
-        if (matrix[i+vertIdx] !== undefined) {
-          while (matrix[i+vertIdx][j] === target[vertIdx]) {
-            // console.log(matrix[i+vertIdx][j]);
-            if (vertIdx === target.length-1) return true;
+        if (board[i+vertIdx] !== undefined) {
+          while (board[i+vertIdx][j] === word[vertIdx]) {
+            if (vertIdx === word.length-1) return true;
             vertIdx ++;
           }
           vertIdx = 1;
         }
-        if (matrix[i][j+horizIdx] !== undefined) {
-          while (matrix[i][j+horizIdx] === target[horizIdx]) {
-            if (horizIdx === target.length-1) return true;
+        if (board[i][j+horizIdx] !== undefined) {
+          while (board[i][j+horizIdx] === word[horizIdx]) {
+            if (horizIdx === word.length-1) return true;
             horizIdx ++;
           }
           horizIdx = 1;
@@ -42,20 +41,20 @@ function wordSearch(matrix, target) {
 };
 
 function test() {
-  let puzzle = [['F', 'A', 'C', 'A'],
+  let board = [['F', 'A', 'C', 'A'],
                 ['O', 'B', 'Q', 'P'],
                 ['A', 'N', 'O', 'E'],
                 ['M', 'A', 'S', 'S']];
   let word = 'FOAM';
-  console.log(wordSearch(puzzle, word));
+  console.log(wordSearch(board, word));
   word = 'NO';
-  console.log(wordSearch(puzzle, word));
+  console.log(wordSearch(board, word));
   word = 'MASS';
-  console.log(wordSearch(puzzle, word));
+  console.log(wordSearch(board, word));
   word = 'PISS';
-  console.log(wordSearch(puzzle, word) === false);
+  console.log(wordSearch(board, word) === false);
   word = 'APES';
-  console.log(wordSearch(puzzle, word));
+  console.log(wordSearch(board, word));
 };
 
 test();
