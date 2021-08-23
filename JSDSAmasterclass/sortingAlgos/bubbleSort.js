bce9c3b749d02868b28bad59ff33fd05fa499ac2
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-08-22 20:34:27 -0400

    completed naive and optimized implementations of bubble sort and wrote detailed tests to compare their performance

---
+++ JSDSAmasterclass/sortingAlgos/bubbleSort.js
@@ -0,0 +1,43 @@
+let {performance} = require('perf_hooks');
+/*
+Implement an iterative bubble sort function
+*/
+
+// naive solution
+function bubbleSortNaive(arr) {
+  for (let i = 0; i < arr.length; i ++) {
+    for (let j = 0; j < arr.length; j ++) {
+      if (arr[j] > arr[j+1]) {
+        [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+      }
+    }
+  }
+  return arr;
+};
+
+// this is the optimized solution
+function bubbleSort(arr) {
+  for (let i = arr.length; i > 0; i --) {
+    for (let j = 0; j < i-1; j ++) {
+      if (arr[j] > arr[j+1]) {
+        [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+      }
+    }
+  }
+  return arr;
+};
+
+function test() {
+  let t1 = performance.now();
+  JSON.stringify(bubbleSortNaive([43, 12, 65, 3, 24, 8, 9])) === JSON.stringify([3, 8, 9, 12, 24, 43, 65]);
+  let t2 = performance.now();
+
+  let t3 = performance.now();
+  JSON.stringify(bubbleSort([43, 12, 65, 3, 24, 8, 9])) === JSON.stringify([3, 8, 9, 12, 24, 43, 65]);
+  let t4 = performance.now();
+
+  console.log(`~~~~~~~~~~~~~~~\n~~~~~~~~~~\n~~~~~\nNaive Solution runtime: ${t2-t1} ms.\n-----------------------------------------------------`);
+  console.log(`Optimized Solution runtime: ${t4-t3} ms.\n~~~~~\n~~~~~~~~~~\n~~~~~~~~~~~~~~~`);
+};
+
+test();
