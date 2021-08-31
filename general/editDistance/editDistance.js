20a359783e8f1e062cc54871c535a260e833cfe9
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-08-31 13:49:53 -0400

    started edit distance

---
+++ general/editDistance/editDistance.js
@@ -0,0 +1,22 @@
+/*
+The edit distance between two strings refers to the minimum number of
+character insertions, deletions, and substitutions required to change one string to the other.
+
+For example, the edit distance between “kitten” and “sitting” is three:
+substitute the “k” for “s”, substitute the “e” for “i”, and append a “g”.
+
+Given two strings, compute the edit distance between them.
+*/
+// input: two strings
+// output: number that is the number of edits
+// constraints: n/a
+// edge cases: n/a
+function editDistance(str1, str2) {
+
+};
+
+function test() {
+
+};
+
+test();
