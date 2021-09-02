73ce69be78a9e05f1c4401461185f2dcdbe10ec8
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-09-02 17:04:08 -0400

    started howSum

---
+++ general/dynamicProgrammingPractice/howSum.js
@@ -0,0 +1,10 @@
+/*
+howSum
+
+write a function howSum(targetSum, numbers) that takes in a targetSum and an array of numbers as arguments
+
+the function should return an array containing any compbination of elements that add up to exactly the targetSum. if there is no combination that adds up to the targetSum, then return null
+
+if there are mutliple combinations possible, you may return any single one.
+*/
+
