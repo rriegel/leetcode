4aa6cfa403af17f6e4f4858527dfcb93d9f26dff
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-09-02 17:03:05 -0400

    finished memoizaed solution of canSum

--- general/dynamicProgrammingPractice/canSum.js
+++ general/dynamicProgrammingPractice/canSum.js
@@ -9,22 +9,41 @@ you may use an element of the array as many times as needed
 
 you may assume that all input numbers are non-negative
 */
-const canSum = (targetSum, nums) => {
+const canSum1 = (targetSum, nums) => {
   if (targetSum === 0) return true;
   if (targetSum < 0) return false;
 
   for (let num of nums) {
     const remainder = targetSum - num;
-    if (canSum(remainder, nums) === true) {
+    if (canSum1(remainder, nums) === true) {
       return true;
     }
   }
   return false;
 };
 
-console.log(canSum(7, [2, 3])); // --> true;
-console.log(canSum(7, [5, 3, 4, 7])); // --> true;
-console.log(canSum(7, [2, 4])); // --> false;
-console.log(canSum(8, [2, 3, 5])); // --> true;
+console.log(canSum1(7, [2, 3])); // --> true;
+console.log(canSum1(7, [5, 3, 4, 7])); // --> true;
+console.log(canSum1(7, [2, 4])); // --> false;
+console.log(canSum1(8, [2, 3, 5])); // --> true;
+
+
+const canSum2 = (targetSum, nums, memo={}) => {
+  if (targetSum in memo) return memo[targetSum];
+
+  if (targetSum === 0) return true;
+  if (targetSum < 0) return false;
+
+  for (let num of nums) {
+    const remainder = targetSum - num;
+    if (canSum2(remainder, nums, memo) === true) {
+      memo[targetSum] = true;
+      return memo[targetSum];
+    }
+  }
+  memo[targetSum] = false;
+  return memo[targetSum];
+};
+
 // this larger input takes a long time
-console.log(canSum(300, [7, 14])); // false
+console.log(canSum2(300, [7, 14])); // false
