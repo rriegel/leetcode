04edc43fddab90a71601bf669a8c7916c5f1665f
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-09-10 15:51:37 -0400

    started happy number . j s

---
+++ grokkingTheCodingInterview/fastAndSlowPointer/happyNumber.js
@@ -0,0 +1,13 @@
+/*
+Any number will be called a happy number if, after repeatedly replacing it with a number equal to the sum of the square of all of its digits, leads us to number ‘1’. All other (not-happy) numbers will never reach ‘1’. Instead, they will be stuck in a cycle of numbers which does not include ‘1’.
+
+Example 1:
+
+Input: 23
+Output: true (23 is a happy number)
+Explanations: Here are the steps to find out that 23 is a happy number:
+2^2 + 3^2 = 4 + 9 = 13
+1^2 + 3^2 = 1 + 9 = 10
+1^2 + 0^2 = 1 + 0 = 1
+*/
+
