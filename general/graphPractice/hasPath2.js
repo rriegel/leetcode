efdc86d5da58bcfdf025ac67341aa305378343b4
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-09-16 15:35:55 -0400

    completed hasPath again and cleaned test up on hasPath 1

--- general/graphPractice/hasPath2.js
+++ general/graphPractice/hasPath2.js
@@ -2,29 +2,36 @@
 has path
 Write a function, hasPath, that takes in an object representing the adjacency list of a directed acyclic graph and two nodes (src, dst). The function should return a boolean indicating whether or not there exists a directed path between the source and destination nodes.
 */
-
+/*
+Complexity
+Time: O(e) --- e = edges OR O(n^2) ... ?
+Space: O(n) --- n = nodes
+*/
+// use a queue for BFS
 function BFSHasPath(graph, src, dst) {
   const queue = [src];
   while (queue.length) {
-    let node = queue.shift();
-    if (node === dst) return true;
-    for (let neighbor of graph[node]) {
+    let curr = queue.shift();
+    if (curr === dst) return true;
+
+    let neighbors = graph[curr];
+    for (let neighbor of neighbors) {
       queue.push(neighbor);
     }
   }
   return false;
 };
-
+// use call stack
 function DFSHasPath(graph, src, dst) {
   if (src === dst) return true;
 
-  for (neighbor of graph[src]) {
-    if (DFSHasPath(graph, neighbor, dst)) {
-      return true;
-    }
+  let neighbors = graph[src];
+  for (let neighbor of neighbors) {
+    if (DFSHasPath(graph, neighbor, dst) === true) return true;
   }
   return false;
 };
+
 
 function test() {
 
@@ -37,8 +44,8 @@ function test() {
     k: []
   };
 
-  console.log(BFSHasPath(graph, 'f', 'k'));
-  console.log(BFSHasPath(graph, 'i', 'h'));
+  console.log(BFSHasPath(graph, 'f', 'k') === true);
+  console.log(BFSHasPath(graph, 'i', 'h') === true);
   console.log(BFSHasPath(graph, 'i', 'f') === false);
   console.log(BFSHasPath(graph, 'f', 'j') === false);
 
@@ -50,7 +57,8 @@ function test() {
     z: [],
   };
 
-  console.log(DFSHasPath(graph, 'v', 'w'));
+  console.log(DFSHasPath(graph, 'v', 'w') === true);
+  console.log(DFSHasPath(graph, 'v', 'y') === false);
 };
 
 test();
