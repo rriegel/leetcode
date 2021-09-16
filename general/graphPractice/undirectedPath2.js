e85560b27f3e22d7c2d24682d73e910b75fea6eb
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-09-16 17:30:44 -0400

    started undirectedPath2

---
+++ general/graphPractice/undirectedPath2.js
@@ -0,0 +1,25 @@
+/*
+undirected path
+
+Write a function, undirectedPath, that takes in an array of edges for an undirected graph and two nodes (nodeA, nodeB). The function should return a boolean indicating whether or not there exists a path between nodeA and nodeB.
+*/
+/*
+Key Ideas
+build the adjacency list
+
+DFS or BFS
+
+remember visited nodes - because it is an undirected graph
+*/
+
+function hasPath() {
+
+};
+
+function buildGraph() {
+
+};
+
+function DFSTraversal() {
+
+};
