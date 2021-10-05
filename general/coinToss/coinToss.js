14fd8d8797552dd4cfd517f2b21df627001fcd66
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-10-05 15:05:23 -0400

    completed unbiased coin toss

---
+++ general/coinToss/coinToss.js
@@ -0,0 +1,32 @@
+/*
+Assume you have access to a function toss_biased() which returns 0 or 1 with a probability that's not 50-50 (but also not 0-100 or 100-0).
+You do not know the bias of the coin.
+
+Write a function to simulate an unbiased coin toss.
+*/
+
+function unbiasedCoinToss() {
+  let rnd = Math.random();
+  if (rnd < 0.5) {
+    return 'H';
+  } else {
+    return 'T';
+  }
+};
+
+function test() {
+  let heads = 0;
+  let tails = 0;
+  for (let i = 0; i < 10000; i ++) {
+    if (unbiasedCoinToss() === 'H') {
+      heads ++;
+    } else {
+      tails ++;
+    }
+  }
+  console.log('Unbiased Coin Toss');
+  console.log(`H: ${((heads/10000)*100).toFixed(2)} %`);
+  console.log(`T: ${((tails/10000)*100).toFixed(2)} %`);
+};
+
+test();
