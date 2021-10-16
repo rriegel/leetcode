cf5cb40ab5ef2078c51391cc6f44f57ab1ceff69
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-10-16 12:00:16 -0400

    completed minColumnsToRemove

---
+++ general/minColumnsToRemove/minColumnsToRemove.js
@@ -0,0 +1,70 @@
+/*
+You are given an N by M 2D matrix of lowercase letters. Determine the minimum number of columns that can be removed to ensure that each row is ordered from top to bottom lexicographically. That is, the letter at each column is lexicographically later as you go down each row. It does not matter whether each row itself is ordered lexicographically.
+
+For example, given the following table:
+
+cba
+daf
+ghi
+This is not ordered because of the a in the center. We can remove the second column to make it ordered:
+
+ca
+df
+gi
+So your function should return 1, since we only needed to remove 1 column.
+
+As another example, given the following table:
+
+abcdef
+Your function should return 0, since the rows are already ordered (there's only one row).
+
+As another example, given the following table:
+
+zyx
+wvu
+tsr
+Your function should return 3, since we would need to remove all the columns to order it.
+*/
+// console.log('b'.charCodeAt(0) - 97);
+
+function minColumnsToRemove(table) {
+  let rm = 0;
+  if (table.length < 2) return rm;
+
+  for (let i = 0; i < table[0].length; i ++) {
+    let compare = table[0][i].charCodeAt(0) - 97;
+    for (let j = 1; j < table.length; j ++) {
+      let current = table[j][i].charCodeAt(0) - 97
+      if (current <= compare) {
+        rm ++;
+        break;
+      } else {
+        compare = current;
+      }
+    }
+  }
+
+  return rm;
+};
+
+function test() {
+  let table1 = [
+    ["c","b","a"],
+    ["d","a","f"],
+    ["g","h","i"]
+  ];
+  let table2 = [
+    ["c","b","a"]
+  ];
+  let table3 = [
+    ["z","y","x"],
+    ["w","v","u"],
+    ["t","s","r"]
+  ];
+
+  console.log(minColumnsToRemove(table1) === 1);
+  console.log(minColumnsToRemove(table2) === 0);
+  console.log(minColumnsToRemove(table3) === 3);
+};
+
+test();
