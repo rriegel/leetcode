cace6d50b9174495f1b08264a2c518311306955e
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-10-24 20:49:31 -0400

    added test case

--- general/longestSequence/longestSequence2.js
+++ general/longestSequence/longestSequence2.js
@@ -28,6 +28,7 @@ function longestConsecutive(nums) {
 
 function test() {
   console.log(longestConsecutive([100, 4, 200, 1, 3, 2]) === 4);
+  console.log(longestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]) === 9);
 };
 
 test();
