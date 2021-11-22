a7aa6a0ef2020bc75cdfb18464448a13ba78f6b3
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-11-22 10:37:33 -0500

    completed reverseWords

---
+++ general/reverseWords/reverseWords.js
@@ -0,0 +1,15 @@
+/*
+Given a string of words delimited by spaces, reverse the words in string. For example, given "hello world here", return "here world hello"
+
+Follow-up: given a mutable string representation, can you perform this operation in-place?
+*/
+
+function reverseWords(str) {
+  return str.split(/\s/).reverse().join(" ");
+};
+
+function test() {
+  console.log(reverseWords("hello world here") === "here world hello");
+};
+
+test();
