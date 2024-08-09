858c819ad361a3e6053fc9485987090070aada7e
rriegel <ryan.j.riegel@gmail.com>
2024-08-09 12:29:25 -0400

    cleanup

--- codeSignalPrep/jumpGame/jumpGame.js
+++ codeSignalPrep/jumpGame/jumpGame.js
@@ -32,9 +32,9 @@ Constraints:
 var canJump = function(nums) {
   let lastGoodPos = nums.length - 1;
   for (let i = nums.length - 1; i >= 0; i --) {
-      if (i + nums[i] >= lastGoodPos) {
-          lastGoodPos = i;
-      }
+    if (i + nums[i] >= lastGoodPos) {
+      lastGoodPos = i;
+    }
   }
   return lastGoodPos === 0;
 };
