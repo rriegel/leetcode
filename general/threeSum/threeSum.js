cb6917a6fc3e71b9fee2098f817b6b2257d778a2
rriegel <ryan.j.riegel@gmail.com>
2024-11-17 23:00:36 -0500

    completed threeSum

---
+++ general/threeSum/threeSum.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+  const triplets = [];
+  nums.sort((a, b) => a - b);
+  for (let left = 0; left < nums.length; left ++) {
+    if (left === 0 || nums[left] !== nums[left-1]) {
+      let target = 0 - nums[left];
+      let mid = left+1;
+      let right = nums.length-1;
+      while (mid < right) {
+        let sum = nums[mid] + nums[right];
+        if (target === sum) {
+          triplets.push([nums[left], nums[mid], nums[right]]);
+          while (nums[mid] === nums[mid+1]) mid ++;
+          while (nums[right] === nums[right-1]) right --;
+          mid ++;
+          right --;
+        } else if (target < sum) {
+          right --;
+        } else {
+          mid ++;
+        }
+      }
+    }
+  }
+  return triplets;
+};
