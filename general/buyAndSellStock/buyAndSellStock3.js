98c7c9f5f406df4ba8aa9a05726fda62484ad8f9
rriegel <ryan.j.riegel@gmail.com>
2024-11-18 21:59:38 -0500

    spacing

--- general/buyAndSellStock/buyAndSellStock3.js
+++ general/buyAndSellStock/buyAndSellStock3.js
@@ -8,11 +8,11 @@ var maxProfit = function(prices) {
   let buyPrice = prices[0];
   
   for (let sellPrice = 1; sellPrice < prices.length; sellPrice ++) {
-      if (prices[sellPrice] < buyPrice) {
-          buyPrice = prices[sellPrice];
-      }
-      currProfit = Math.max(currProfit, prices[sellPrice] - buyPrice);
-      maxProfit = Math.max(maxProfit, currProfit);
+    if (prices[sellPrice] < buyPrice) {
+      buyPrice = prices[sellPrice];
+    }
+    currProfit = Math.max(currProfit, prices[sellPrice] - buyPrice);
+    maxProfit = Math.max(maxProfit, currProfit);
   }
   return maxProfit;
 };
