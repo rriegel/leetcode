4b7498b5bd67474820f4b366b11289c3fd3b4c8b
rriegel <ryan.j.riegel@gmail.com>
2024-11-18 21:59:12 -0500

    completed buy and sell stock

---
+++ general/buyAndSellStock/buyAndSellStock3.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  let maxProfit = 0;
+  let currProfit = 0;
+  let buyPrice = prices[0];
+  
+  for (let sellPrice = 1; sellPrice < prices.length; sellPrice ++) {
+      if (prices[sellPrice] < buyPrice) {
+          buyPrice = prices[sellPrice];
+      }
+      currProfit = Math.max(currProfit, prices[sellPrice] - buyPrice);
+      maxProfit = Math.max(maxProfit, currProfit);
+  }
+  return maxProfit;
+};
